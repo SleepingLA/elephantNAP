929ae5cf04c471305736db24b506f0180d87799f
SleepingLA <sleepingLA@github.com>
2017-11-06 16:18:13 +0000

    replace platform_stop

--- bin/platform_stop.sh
+++ bin/platform_stop.sh
@@ -1,74 +1,20 @@
 #! /bin/bash
 
-# platform stop try to stop everything, 
-# later you can select to restart different service with start
-# and with a modified platform.conf
+source ~/.bash_profile
+#
+# read conf
+# change platform.conf to launch subset of  initially configured services
+file=${NAPDIR}/platform.conf
+# inverse order for stopping services
+liste=$(tac $file | grep -v -e '#')
 
+for service in $liste
+do
+ echo
+ echo Starting $service 
+ #echo ${NAPDIR}/bin/start_${service}.sh
+ echo ${NAPDIR}/bin/platform_service.sh stop ${service}
+ ${NAPDIR}/bin/platform_service.sh stop ${service}
 
-
-stop_service(){
-
-
-
-#zeppelin
-if [ -e /usr/local/zeppelin ] ; then
-	#${HOME}/zeppelin/bin/zeppelin-daemon.sh stop
-        cd ${NAPDIR}/
-        ./bin/stop_zeppelin.sh
-fi
-
-# presto
-if [ -e /usr/local/presto ] ; then
-        cd ${NAPDIR}/
-        ./bin/stop_presto.sh
-fi
-
-
-# hive
-if [ -e ${HOME}/hive-metastore ] ; then
-        ./bin/stop_hive.sh
-fi
-
-
-
-
-
-# HBASE?
-
-if [ -e /usr/local/hbase ] ; then
-        echo "repartir Hbase a la main au besoin"
-        cd ${NAPDIR}/
-        ./bin/stop_hbase.sh
-fi
-
-
-#spark
-/usr/local/spark/sbin/stop-all.sh
-
-# hadoop
-/usr/local/hadoop/sbin/stop-dfs.sh
-
-# cassandra
-temp=$(ls /var/lib/cassandra | wc -l)
-if [ $temp -gt 0 ] ; then
-        cd ${NAPDIR}/
-        ./bin/stop_cassandra.sh
-fi
-
-
-}
-
-
-
-
-# cassandra
-# Hue livy
-# TODO
-
-
-###############################################
-
-stop_service
-
-
+done
 
