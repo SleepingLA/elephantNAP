ce2ce11f947b0df5ee5e54a194f9f467f65083ea
SleepingLA <leclaire.louis-alexandre@ireq.ca>
2018-02-14 03:33:17 +0000

    update packages

--- platform_bin/download_bin.sh
+++ platform_bin/download_bin.sh
@@ -8,31 +8,33 @@ chmod u+w .
 #if true; then
 if false; then
 
- wget http://apache.mirror.vexxhost.com/hadoop/common/hadoop-2.7.4/hadoop-2.7.4.tar.gz
- wget http://apache.mirror.vexxhost.com/spark/spark-2.2.0/spark-2.2.0-bin-hadoop2.7.tgz
- wget http://apache.mirror.vexxhost.com/hive/hive-2.3.0/apache-hive-2.3.0-bin.tar.gz
- wget http://apache.mirror.vexxhost.com/hbase/1.2.6/hbase-1.2.6-bin.tar.gz
+ wget http://apache.mirror.vexxhost.com/hadoop/common/hadoop-2.7.5/hadoop-2.7.5.tar.gz
+ wget http://apache.mirror.vexxhost.com/spark/spark-2.2.1/spark-2.2.1-bin-hadoop2.7.tgz
+ wget http://apache.mirror.vexxhost.com/hive/hive-2.3.2/apache-hive-2.3.2-bin.tar.gz
+ wget http://apache.mirror.vexxhost.com/hbase/1.4.1/hbase-1.4.1-bin.tar.gz
  wget http://apache.mirror.vexxhost.com/zeppelin/zeppelin-0.7.3/zeppelin-0.7.3-bin-all.tgz
 
  wget http://apache.mirror.vexxhost.com/ignite/2.3.0/apache-ignite-fabric-2.3.0-bin.zip
 
- wget https://repo1.maven.org/maven2/com/facebook/presto/presto-server/0.187/presto-server-0.187.tar.gz
- wget https://repo1.maven.org/maven2/com/facebook/presto/presto-cli/0.187/presto-cli-0.187-executable.jar
+ wget https://repo1.maven.org/maven2/com/facebook/presto/presto-server/0.194/presto-server-0.194.tar.gz
+ wget https://repo1.maven.org/maven2/com/facebook/presto/presto-cli/0.194/presto-cli-0.194-executable.jar
+
+ exit
 fi
 
 
 #place in ansible tree
 
 mkdir ${NAPDIR}/ansible/roles/hadoop/files
-mv hadoop-2.7.4.tar.gz ${NAPDIR}/ansible/roles/hadoop/files
-mv spark-2.2.0-bin-hadoop2.7.tgz ${NAPDIR}/ansible/roles/hadoop/files
-mv hbase-1.2.6-bin.tar.gz ${NAPDIR}/ansible/roles/hbase/templates/
-mv apache-hive-2.3.0-bin.tar.gz ${NAPDIR}/ansible/roles/hive/templates/
+mv hadoop-2.7.5.tar.gz ${NAPDIR}/ansible/roles/hadoop/files
+mv spark-2.2.1-bin-hadoop2.7.tgz ${NAPDIR}/ansible/roles/hadoop/files
+mv hbase-1.4.1-bin.tar.gz ${NAPDIR}/ansible/roles/hbase/templates/
+mv apache-hive-2.3.2-bin.tar.gz ${NAPDIR}/ansible/roles/hive/templates/
 mkdir ${NAPDIR}/ansible/roles/zeppelin/files
 mv zeppelin-0.7.3-bin-all.tgz ${NAPDIR}/ansible/roles/zeppelin/files
 mkdir ${NAPDIR}/ansible/roles/presto/files
-mv presto-server-0.185.tar.gz ${NAPDIR}/ansible/roles/presto/files
-mv presto-cli-0.185-executable.jar ${NAPDIR}/ansible/roles/presto/files
+mv presto-server-0.194.tar.gz ${NAPDIR}/ansible/roles/presto/files
+mv presto-cli-0.194-executable.jar ${NAPDIR}/ansible/roles/presto/files
 
 mkdir ${NAPDIR}/ansible/roles/ignite/files
 mv apache-ignite-fabric-2.3.0-bin.zip ${NAPDIR}/ansible/roles/ignite/files
