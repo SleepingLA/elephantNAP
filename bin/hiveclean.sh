479a9fce768c3c1bc0ae7134e0d638e68bfdba7f
SleepingLA <leclaire.louis-alexandre@ireq.ca>
2018-03-07 18:54:32 +0000

    hiveclean tool updated

--- bin/hiveclean.sh
+++ bin/hiveclean.sh
@@ -1,9 +1,13 @@
 
 #arret des services
-${NAPDIR}/bin/stop_hive.sh
+#${NAPDIR}/bin/stop_hive.sh
+${NAPDIR}/bin/platform_service.sh stop hive
 
 # effacer hdfs warehouse
 
 hdfs dfs -rm -r /user/hive/warehouse
+# clean tmp
+hdfs dfs -rm -r /tmp/hive/*
+
 # effacer metastore
 rm -fr  $HOME/hive-metastore
