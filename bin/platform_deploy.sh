e9db2ba4084e54d6913c50543013ecf07c45bc3f
sleepingla <sleepingla@github.com>
2018-05-20 00:55:23 +0000

    typo

--- bin/platform_deploy.sh
+++ bin/platform_deploy.sh
@@ -20,7 +20,7 @@ echo configuration de base du cluster
 ${NAPDIR}/bin/provision_and_configure.sh
 
 # individual service platform.conf
-${NAPDIR}/bin/configure_service.sh
+${NAPDIR}/bin/configure_services.sh
 
 # start 
 ${NAPDIR}/bin/platform_start.sh
