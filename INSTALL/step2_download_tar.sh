b96d525d48c9c6b04f693985a3cfcd467db66f72
SleepingLA <leclaire.louis-alexandre@ireq.ca>
2018-09-26 12:44:28 +0000

    update to spark 2.3.2

--- INSTALL/step2_download_tar.sh
+++ INSTALL/step2_download_tar.sh
@@ -8,8 +8,8 @@ chmod u+w .
 #if true; then
 if false; then
 
- wget http://apache.mirror.vexxhost.com/hadoop/common/hadoop-2.7.6/hadoop-2.7.6.tar.gz
- wget http://apache.mirror.vexxhost.com/spark/spark-2.3.1/spark-2.3.0-bin-hadoop2.7.tgz
+ wget http://apache.mirror.vexxhost.com/hadoop/common/hadoop-2.8.5/hadoop-2.8.5.tar.gz
+ wget http://apache.mirror.vexxhost.com/spark/spark-2.3.2/spark-2.3.2-bin-hadoop2.7.tgz
 
  wget http://apache.mirror.vexxhost.com/hive/hive-2.3.3/apache-hive-2.3.3-bin.tar.gz
  wget http://apache.mirror.vexxhost.com/hbase/2.0.0/hbase-2.0.0-bin.tar.gz
@@ -17,8 +17,8 @@ if false; then
 
  wget http://apache.mirror.vexxhost.com/ignite/2.3.0/apache-ignite-fabric-2.3.0-bin.zip
 
- wget --no-check-certificate https://repo1.maven.org/maven2/com/facebook/presto/presto-server/0.204/presto-server-0.204.tar.gz
- wget --no-check-certificate  https://repo1.maven.org/maven2/com/facebook/presto/presto-cli/0.204/presto-cli-0.204-executable.jar
+ wget --no-check-certificate https://repo1.maven.org/maven2/com/facebook/presto/presto-server/0.211/presto-server-0.204.tar.gz
+ wget --no-check-certificate  https://repo1.maven.org/maven2/com/facebook/presto/presto-cli/0.211/presto-cli-0.204-executable.jar
 
  exit
 fi
@@ -28,8 +28,8 @@ fi
 #must create dir: git dont arch empty files dir
 
 mkdir ${NAPDIR}/ansible/roles/hadoop/files
-mv hadoop-2.7.6.tar.gz ${NAPDIR}/ansible/roles/hadoop/files
-mv spark-2.3.1-bin-hadoop2.7.tgz ${NAPDIR}/ansible/roles/hadoop/files
+mv hadoop-2.8.5.tar.gz ${NAPDIR}/ansible/roles/hadoop/files
+mv spark-2.3.2-bin-hadoop2.7.tgz ${NAPDIR}/ansible/roles/hadoop/files
 
 mv hbase-2.0.0-bin.tar.gz ${NAPDIR}/ansible/roles/hbase/templates/
 mv apache-hive-2.3.3-bin.tar.gz ${NAPDIR}/ansible/roles/hive/templates/
@@ -37,8 +37,8 @@ mv apache-hive-2.3.3-bin.tar.gz ${NAPDIR}/ansible/roles/hive/templates/
 mkdir ${NAPDIR}/ansible/roles/zeppelin/files
 mv zeppelin-0.8.0-bin-all.tgz ${NAPDIR}/ansible/roles/zeppelin/files
 mkdir ${NAPDIR}/ansible/roles/presto/files
-mv presto-server-0.204.tar.gz ${NAPDIR}/ansible/roles/presto/files
-mv presto-cli-0.204-executable.jar ${NAPDIR}/ansible/roles/presto/files
+mv presto-server-0.211.tar.gz ${NAPDIR}/ansible/roles/presto/files
+mv presto-cli-0.211-executable.jar ${NAPDIR}/ansible/roles/presto/files
 
 mkdir ${NAPDIR}/ansible/roles/ignite/files
 mv apache-ignite-fabric-2.3.0-bin.zip ${NAPDIR}/ansible/roles/ignite/files
